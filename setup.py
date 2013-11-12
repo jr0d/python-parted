f7a923076a0a1513344d1b505edf12ac002db6db
Jared Rodriguez <jared.rodriguez@rackspace.com>
2013-11-12 09:37:55 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-parted',
+    version='0.0.2',
+    packages=['parted'],
+    url='',
+    license='',
+    author='jared',
+    author_email='',
+    description=''
+)
